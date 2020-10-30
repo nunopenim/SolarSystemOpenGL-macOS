ccac7f452144a9186c2bea000fcabf1632ad74fa
nunopenim <nuno.penim@gmail.com>
2020-10-30 09:43:38 +0000

    Shader.hh: fixed GLM references

--- include/Shader.hh
+++ include/Shader.hh
@@ -12,9 +12,9 @@
 #include <GLFW/glfw3.h>
 
 // GLM Stuff
-#include <glm/glm.hpp>
+#include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
-#include <glm/gtc/type_ptr.hpp>
+#include "glm/gtc/type_ptr.hpp"
 
 class Shader
 {
