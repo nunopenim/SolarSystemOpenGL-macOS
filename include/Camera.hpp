b9674a1a8fc9a758bf31589721d28482b66e477e
nunopenim <nuno.penim@gmail.com>
2020-10-30 09:41:39 +0000

    general: added GLM lib and fixed camera.hpp

--- include/Camera.hpp
+++ include/Camera.hpp
@@ -1,8 +1,8 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
 
 #include <vector>
 
