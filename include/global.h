ebdda6e0f1112ea23603342846bc6f9493743fa7
nunopenim <nuno.penim@gmail.com>
2020-10-30 09:35:55 +0000

    general: main code from Linux port

---
+++ include/global.h
@@ -0,0 +1,18 @@
+// GL/GLFW Stuff
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+// GLM Stuff
+#include <glm/glm.hpp>
+#include "glm/gtc/matrix_transform.hpp"
+#include <glm/gtc/type_ptr.hpp>
+
+// My stuff
+#include "Camera.hpp"
+
+#ifndef GLOBAL
+#define GLOBAL
+
+extern Camera camera(glm::vec3(0.0f, 45.0f, 0.0f));
+
+#endif
