4cf3ef21dc676b0864e423b506dad0f505402757
nunopenim <nuno.penim@gmail.com>
2020-10-30 09:42:35 +0000

    global.h: fixed glm references

--- include/global.h
+++ include/global.h
@@ -3,9 +3,9 @@
 #include <GLFW/glfw3.h>
 
 // GLM Stuff
-#include <glm/glm.hpp>
+#include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
-#include <glm/gtc/type_ptr.hpp>
+#include "glm/gtc/type_ptr.hpp"
 
 // My stuff
 #include "Camera.hpp"
